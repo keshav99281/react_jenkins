7acfbbc15963e2eea09513db49ba08ec08ba1721
keshav99281 <keshavgamer18@gmail.com>
2025-04-05 15:22:36 +0530

    Second commit

---
+++ nodeterraform/main.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "rgasp" {
+  location = var.location
+  name = var.resource_group_name
+}
+
+resource "azurerm_service_plan" "plan" {
+  location = azurerm_resource_group.rgasp.location
+  name = var.service_plan_name
+  os_type = var.os
+  resource_group_name = azurerm_resource_group.rgasp.name
+  sku_name = var.pricing_plan
+}
+
+resource "azurerm_linux_web_app" "serviceApp" {
+  service_plan_id = azurerm_service_plan.plan.id
+  location = azurerm_service_plan.plan.location
+  name = var.linux_web_app_name
+  resource_group_name = azurerm_resource_group.rgasp.name
+  site_config {
+    application_stack {
+      node_version = var.version
+    }
+  }
+}
+
---
+++ nodeterraform/variables.tf
@@ -0,0 +1,51 @@
+variable "subscription_id" {
+  description = "Subscription id of the account"
+  type = string
+  default = "4bcf59ba-7d66-4950-9c1d-8786a2bf6489"
+}
+
+variable "location" {
+    description = "location of service"
+    type = string
+    default = "eastus"
+}
+
+variable "resource_group_name" {
+  description = "resource group name"
+  type = string
+  default = "rg-jenkins"
+}
+
+variable "linux_web_app_name" {
+  description = "name of the app service"
+  type = string
+  default = "reacttjenkinskeshav"
+}
+
+# variable "linux_web_app_slot_name" {
+#   description = "name of windows web app slot"
+#   type = string
+#   default = "dotnetslotkeshav"
+# }
+
+variable "service_plan_name" {
+  description = "Name of the service plan"
+  type = string
+  default = "appserviceplankeshav"
+}
+
+variable "os" {
+  description = "Operating system"
+  type = string
+  default = "Linux"
+}
+
+variable "pricing_plan" {
+  description = "Pricing plan of the azure service plan"
+  type = string
+  default = "F1"
+}
+
+variable "version" {
+  default = "22"
+}
